fbf1fbc4d3e1993592630558b3e3507559b72397
William Olsson <burrken55@gmail.com>
2021-04-01 10:57:50 +0200

    Add project files.

---
+++ Nätverksprogrammering/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Nätverksprogrammering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            byte[] textBytes = Encoding.unicode.GetBytes("hej");
+            string text = Encoding.Unicode.Getstring(textBytes);
+            byte[] talBytes = BitConverter.GetBytes(7);
+            int tal = BitConverter.ToInt32(talBytes, 0);
+        }
+    }
+}
